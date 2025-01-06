3fa8029b7ac52f29bdb13d3b4531c5e2ac51f590
TagCopperLight <tagcopperlight@gmail.com>
2025-01-06 14:24:59 +0100

    Structure Cell

---
+++ minesweeper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    int x;
+    int y;
+    int adjacentMines;
+    bool isMine;
+    bool isRevealed;
+    bool isFlagged;
+    Cell** adjacentCells;
+} Cell;
